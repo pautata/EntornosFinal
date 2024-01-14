d5b348b923bfb0b3aa889ec675ed24c67d3a6eb2
Gianqui08 <152377395+Gianqui08@users.noreply.github.com>
2024-01-14 01:41:14 +0100

    Cubo volumen y area

--- src/entornospack/Cubo.java
+++ src/entornospack/Cubo.java
@@ -1,22 +1,30 @@
 package entornospack;
-
 import java.util.Scanner;
-
 public class Cubo {
 
-	 public static double calcularVolumenCubo(double lado) {
-	        return Math.pow(lado, 3);
-	    }
+   
+    public static double calcularVolumenCubo(double lado) {
+        return Math.pow(lado, 3);
+    }
 
-	    public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
+    public static double calcularAreaCubo(double lado) {
+        return 6 * Math.pow(lado, 2);
+    }
 
-	        System.out.print("Ingrese la longitud del lado del cubo: ");
-	        double lado = scanner.nextDouble();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	        // Llamamos al método estático para calcular el volumen del cubo
-	        double volumen = calcularVolumenCubo(lado);
+        System.out.print("Ingrese la longitud del lado del cubo: ");
+        double lado = scanner.nextDouble();
 
-	        System.out.println("El volumen del cubo es: " + volumen);
-	    }
-	}
+
+        double volumen = calcularVolumenCubo(lado);
+        System.out.println("El volumen del cubo es: " + volumen);
+
+        
+        double area = calcularAreaCubo(lado);
+        System.out.println("El área del cubo es: " + area);
+        
+        scanner.close();
+    }
+}
